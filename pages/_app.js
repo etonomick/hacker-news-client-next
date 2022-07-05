b9335bfd57c3af9304b53dee19b70b4facae45ae
Valery Stepanov <etonomick@gmail.com>
2022-07-05 16:15:53 +0000

    Tabs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import Head from "next/head"
 import "../styles/globals.css"
 
 export default function App({ Component, pageProps }) {
     return (
-        <Component {...pageProps} />
+        <>
+            <Head>
+                <title>Hacker News Clinet</title>
+            </Head>
+            <div className="flex flex-col p-5 gap-5" style={{ WebkitTapHighlightColor: 'transparent' }}>
+                <h1 className="text-2xl font-medium">Hacker News</h1>
+                <Component {...pageProps} />
+            </div>
+        </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,36 @@
-import { Tab } from "@headlessui/react"
+import { Tab, Panel } from "@headlessui/react"
+import { useEffect, useState } from "react"
 
 export default function Index() {
+
+    const tabs = ["Best", "New"]
+    const [currentTabIndex, setCurrentTabIndex] = useState(0)
+
+    useEffect(() => {
+
+    }, [currentTabIndex])
+
     return (
-        <></>
+        <Tab.Group
+            onChange={(index) => {
+                setCurrentTabIndex(index)
+            }}>
+            <Tab.List className="flex">
+                {tabs.map(tab => (
+                    <Tab
+                        key={tab}
+                        className={({ selected }) => `w-full py-1 border-b-2 ${selected ? "outline:none focus:outline-none border-black font-medium" : "border-gray-300"}`}>{tab}</Tab>
+                ))}
+            </Tab.List>
+            <Tab.Panels>
+                {tabs.map((tab, index) => {
+                    return (
+                        <Tab.Panel>
+                            {index}
+                        </Tab.Panel>
+                    )
+                })}
+            </Tab.Panels>
+        </Tab.Group>
     )
 }
