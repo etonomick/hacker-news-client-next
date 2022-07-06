11fde2d6691cda56e098ae5367b49320687c168e
Valery Stepanov <etonomick@gmail.com>
2022-07-06 07:29:32 +0000

    Tabs

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,33 @@
-import { Tab, Panel } from "@headlessui/react"
+import { Tab } from "@headlessui/react"
 import { useEffect, useState } from "react"
 
 export default function Index() {
 
-    const tabs = ["Best", "New"]
+    const tabs = [
+        {
+            title: "Best",
+            type: "best"
+        },
+        {
+            title: "New",
+            type: "new"
+        }
+    ]
+
     const [currentTabIndex, setCurrentTabIndex] = useState(0)
 
-    useEffect(() => {
+    const [stories, setStories] = useState({})
 
+    const fetchStories = async (type) => {
+        const res = await fetch(`/api/stories/${type}`).then(res => res.json())
+        setStories({ ...stories, [`${type}`]: res })
+    }
+
+    useEffect(() => {
+        const type = tabs[currentTabIndex].type
+        if (!stories[type]) {
+            fetchStories(type)
+        }
     }, [currentTabIndex])
 
     return (
@@ -19,14 +39,22 @@ export default function Index() {
                 {tabs.map(tab => (
                     <Tab
                         key={tab}
-                        className={({ selected }) => `w-full py-1 border-b-2 ${selected ? "outline:none focus:outline-none border-black font-medium" : "border-gray-300"}`}>{tab}</Tab>
+                        className={({ selected }) => `w-full py-1 border-b-2 ${selected ? "outline:none focus:outline-none border-black font-medium" : "border-gray-300"}`}>{tab.title}</Tab>
                 ))}
             </Tab.List>
             <Tab.Panels>
                 {tabs.map((tab, index) => {
                     return (
                         <Tab.Panel>
-                            {index}
+                            {stories[tab.type] 
+                            ? 
+                            <div>
+                                {stories[tab.type].map(story => (
+                                    <div>{story.title}</div>
+                                ))}
+                            </div> 
+                            : 
+                            <div>Loading...</div>}
                         </Tab.Panel>
                     )
                 })}
